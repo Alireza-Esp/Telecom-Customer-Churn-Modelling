344b4a90f42e38969ffc191ef428fd8c7f87060b
Alireza Esmaeilpour <alireza.esp07@gmail.com>
2025-07-24 23:30:02 +0330

    Continue dashboarding - Add requierments.txt

--- dashboard/main.py
+++ dashboard/main.py
@@ -3,12 +3,6 @@ from data import *
 from charts import *
 from styles import *
 
-if "first_run" not in st.session_state:
-    st.session_state.first_run = True
-else:
-    st.session_state.first_run = False
-
-
 st.set_page_config(
     page_title="Telecom Customer Churn Dashboard",
     page_icon="📊",
@@ -30,7 +24,7 @@ with st.sidebar:
         st.markdown("- [Count of Unique Values](#how-many-of-each-unique-value-of-the-indices-are-there)")
         st.markdown("- [Distribution of Numerical Indices](#how-is-the-distribution-of-each-numerical-index)")
         st.markdown("- [Fraction of Total Values](#what-fraction-of-the-total-values-of-a-numerical-index-does-each-unique-value-of-a-categorical-index-make-up)")
-        st.markdown("- [Distribution of Numerical indices Over Categorical indices](#how-is-the-distribution-of-numerical-indices-over-categorical-indices)")
+        st.markdown("- [Distribution of Numerical Indices Over Categorical indices](#how-is-the-distribution-of-numerical-indices-over-categorical-indices)")
         st.markdown("- [Distribution of Numerical Indices Relative to Each Other](#how-are-the-numerical-indices-distributed-relative-to-each-other)")
     
     st.container(height=10, border=False)
@@ -46,14 +40,16 @@ with st.sidebar:
         st.markdown(" - [Kaggle](https://www.kaggle.com/alirezaesmaeilpour)")
         st.markdown(" - [Website](https://alireza-esp.ir/)")
 
+
 data = get_main_dataset()
 cities_density = get_cities_density_dataset()
+
 
 sec1 = st.container(border=True)
 with sec1:
     st.markdown("### What is the geographic distribution of customers in California?")
     
-    sec1_col1, sec1_col2= st.columns((1,1), gap="small", vertical_alignment="center")
+    sec1_col1, sec1_col2 = st.columns((1,1), gap="small", vertical_alignment="center")
     
     with sec1_col1:
         scatter_map_plot1 = get_customers_scatter_map(
@@ -79,38 +75,72 @@ st.container(height=20, border=False)
 
 categorical_columns, numerical_columns = get_categorical_numerical_columns(data=data)
 
-
 sec2 = st.container(border=True)
 with sec2:
     st.markdown("### How many of each unique value of the indices are there?")
+    st.container(height=20, border=False)
     
     sec2_col1, sec2_col2, sec2_col3 = st.columns((1,1,1), gap="small")
     
+    select_main_category_text = "Select a main category:"
+    select_secondary_category_text = "Select a secondary category:"
     with sec2_col1:
+        histogram_plot1_main_category = st.selectbox(
+            label=select_main_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Internet Service"),
+            key="histogram_plot1_main_category"
+        )
+        histogram_plot1_secondary_category = st.selectbox(
+            label=select_secondary_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Churn Label"),
+            key="histogram_plot1_secondary_category"
+        )
         histogram_plot1 = get_histogram_plot(
             data=data,
-            category_x="Internet Service",
-            category_color="Churn Label",
+            category_x=histogram_plot1_main_category,
+            category_color=histogram_plot1_secondary_category,
         )
         st.plotly_chart(histogram_plot1, key="histogram_plot1")
-        histogram_plot1_main_category = st.selectbox(
-            label="Select a main category:",
-            options=categorical_columns
-        )
     
     with sec2_col2:
+        histogram_plot2_main_category = st.selectbox(
+            label=select_main_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Customer Status"),
+            key="histogram_plot2_main_category"
+        )
+        histogram_plot2_secondary_category = st.selectbox(
+            label=select_secondary_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Referred a Friend"),
+            key="histogram_plot2_secondary_category"
+        )
         histogram_plot2 = get_histogram_plot(
             data=data,
-            category_x="Gender",
-            category_color="Churn Label",
+            category_x=histogram_plot2_main_category,
+            category_color=histogram_plot2_secondary_category,
         )
         st.plotly_chart(histogram_plot2, key="histogram_plot2")
     
     with sec2_col3:
+        histogram_plot3_main_category = st.selectbox(
+            label=select_main_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Contract"),
+            key="histogram_plot3_main_category"
+        )
+        histogram_plot3_secondary_category = st.selectbox(
+            label=select_secondary_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Tech Support"),
+            key="histogram_plot3_secondary_category"
+        )
         histogram_plot3 = get_histogram_plot(
             data=data,
-            category_x="Gender",
-            category_color="Churn Label",
+            category_x=histogram_plot3_main_category,
+            category_color=histogram_plot3_secondary_category
         )
         st.plotly_chart(histogram_plot3, key="histogram_plot3")
     
@@ -119,30 +149,69 @@ st.container(height=20, border=False)
 sec3 = st.container(border=True)
 with sec3:
     st.markdown("### How is the distribution of each numerical index?")
+    st.container(height=20, border=False)
     
     sec3_col1, sec3_col2, sec3_col3 = st.columns((1,1,1), gap="small")
     
+    select_numeric_feature_text = "Select a numeric feature:"
+    select_category_text = "Select a category:"
     with sec3_col1:
+        univariate_box_plot1_numeric_feature = st.selectbox(
+            label=select_numeric_feature_text,
+            options=numerical_columns,
+            index=numerical_columns.index("Monthly Charges"),
+            key="univariate_box_plot1_numeric_feature"
+        )
+        univariate_box_plot1_category = st.selectbox(
+            label=select_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Unlimited Data"),
+            key="univariate_box_plot1_category"
+        )
         univariate_box_plot1 = get_univariate_box_plot(
             data=data,
-            numeric_axis="Number of Referrals",
-            category_color="Churn Label",
+            numeric_axis=univariate_box_plot1_numeric_feature,
+            category_color=univariate_box_plot1_category,
         )
         st.plotly_chart(univariate_box_plot1, key="univariate_box_plot1")
     
     with sec3_col2:
+        univariate_box_plot2_numeric_feature = st.selectbox(
+            label=select_numeric_feature_text,
+            options=numerical_columns,
+            index=numerical_columns.index("Number of Referrals"),
+            key="univariate_box_plot2_numeric_feature"
+        )
+        univariate_box_plot2_category = st.selectbox(
+            label=select_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Offer"),
+            key="univariate_box_plot2_category"
+        )
         univariate_box_plot2 = get_univariate_box_plot(
             data=data,
-            numeric_axis="Number of Referrals",
-            category_color="Churn Label",
+            numeric_axis=univariate_box_plot2_numeric_feature,
+            category_color=univariate_box_plot2_category,
         )
         st.plotly_chart(univariate_box_plot2, key="univariate_box_plot2")
     
     with sec3_col3:
+        univariate_box_plot3_numeric_feature = st.selectbox(
+            label=select_numeric_feature_text,
+            options=numerical_columns,
+            index=numerical_columns.index("Total Long Distance Charges"),
+            key="univariate_box_plot3_numeric_feature"
+        )
+        univariate_box_plot3_category = st.selectbox(
+            label=select_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Gender"),
+            key="univariate_box_plot3_category"
+        )
         univariate_box_plot3 = get_univariate_box_plot(
             data=data,
-            numeric_axis="Number of Referrals",
-            category_color="Churn Label",
+            numeric_axis=univariate_box_plot3_numeric_feature,
+            category_color=univariate_box_plot3_category,
         )
         st.plotly_chart(univariate_box_plot3, key="univariate_box_plot3")
     
@@ -151,30 +220,67 @@ st.container(height=20, border=False)
 sec4 = st.container(border=True)
 with sec4:
     st.markdown("### What fraction of the total values of a numerical index does each unique value of a categorical index make up?")
+    st.container(height=20, border=False)
     
     sec4_col1, sec4_col2, sec4_col3 = st.columns((1,1,1), gap="small")
     
     with sec4_col1:
+        pie_plot1_numeric_feature = st.selectbox(
+            label=select_numeric_feature_text,
+            options=numerical_columns,
+            index=numerical_columns.index("Tenure Months"),
+            key="pie_plot1_numeric_feature"
+        )
+        pie_plot1_category = st.selectbox(
+            label=select_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Churn Label"),
+            key="pie_plot1_category"
+        )
         pie_plot1 = get_pie_chart(
             data=data,
-            category="Churn Label",
-            numeric="Total Revenue",
+            category=pie_plot1_category,
+            numeric=pie_plot1_numeric_feature,
         )
         st.plotly_chart(pie_plot1, key="pie_plot1")
     
     with sec4_col2:
+        pie_plot2_numeric_feature = st.selectbox(
+            label=select_numeric_feature_text,
+            options=numerical_columns,
+            index=numerical_columns.index("Total Extra Data Charges"),
+            key="pie_plot2_numeric_feature"
+        )
+        pie_plot2_category = st.selectbox(
+            label=select_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Streaming TV"),
+            key="pie_plot2_category"
+        )
         pie_plot2 = get_pie_chart(
             data=data,
-            category="Churn Label",
-            numeric="Total Revenue",
+            category=pie_plot2_category,
+            numeric=pie_plot2_numeric_feature,
         )
         st.plotly_chart(pie_plot2, key="pie_plot2")
     
     with sec4_col3:
+        pie_plot3_numeric_feature = st.selectbox(
+            label=select_numeric_feature_text,
+            options=numerical_columns,
+            index=numerical_columns.index("Total Charges"),
+            key="pie_plot3_numeric_feature"
+        )
+        pie_plot3_category = st.selectbox(
+            label=select_category_text,
+            options=categorical_columns,
+            index=categorical_columns.index("Contract"),
+            key="pie_plot3_category"
+        )
         pie_plot3 = get_pie_chart(
             data=data,
-            category="Churn Label",
-            numeric="Total Revenue",
+            category=pie_plot3_category,
+            numeric=pie_plot3_numeric_feature,
         )
         st.plotly_chart(pie_plot3, key="pie_plot3")
     
