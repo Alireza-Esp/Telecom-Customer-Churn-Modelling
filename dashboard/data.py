a29b32c89ec906f9a41afee3730d86fedb563cdb
Alireza Esmaeilpour <alireza.esp07@gmail.com>
2025-07-24 17:36:05 +0330

    Continue dashboarding

--- dashboard/data.py
+++ dashboard/data.py
@@ -4,8 +4,23 @@ import pandas as pd
 
 @st.cache_data
 def get_main_dataset():
-    return pd.read_csv("https://github.com/Alireza-Esp/Telecom-Customer-Churn-Modelling/raw/refs/heads/main/data/telecom-customer-churn-v2.csv")
+    data = pd.read_csv("https://github.com/Alireza-Esp/Telecom-Customer-Churn-Modelling/raw/refs/heads/main/data/telecom-customer-churn-v2.csv")
+    data.fillna("NULL", inplace=True)
+    return data
 
-st.cache_data
+@st.cache_data
 def get_cities_density_dataset():
-    return pd.read_csv("")
+    return pd.read_csv("https://github.com/Alireza-Esp/Telecom-Customer-Churn-Modelling/raw/refs/heads/main/data/cities-density.csv")
+
+@st.cache_data
+def get_categorical_numerical_columns(data: pd.DataFrame):
+    categorical_columns = []
+    numerical_columns = []
+    
+    for i in data.columns:
+        if data[i].dtype == "object":
+            categorical_columns.append(i)
+        else:
+            numerical_columns.append(i)
+    
+    return [categorical_columns, numerical_columns]
